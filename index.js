a52b89ca14928944b94bdeb218027d05fdec8541
yash123wanth <guthikondayashwanth897@gmail.com>
2025-07-23 20:45:32 +0530

    commit

--- index.js
+++ index.js
@@ -3,47 +3,57 @@ const { dbConnection } = require("./dbConnection");
 
 let app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/student-read", async (req,res)=>{
-  let myDB = await dbConnection()
-  let studentCollection = myDB.collection("student")
-  let data = await studentCollection.find().toArray()
-    let resObj={
+app.get("/student-read", async (req, res) => {
+  let myDB = await dbConnection();
+  let studentCollection = myDB.collection("student");
+  let data = await studentCollection.find().toArray();
+  let resObj = {
     status: 1,
     msg: "studentinf",
-    data
-  }
-  res.send(resObj)
+    data,
+  };
+  res.send(resObj);
 
-  res.send("App vieW API")
-})
+  res.send("App vieW API"); //uiebvbsB
+});
+app.get("/student-read", async (req, res) => {
+  let myDB = await dbConnection();
+  let studentCollection = myDB.collection("student");
+  let data = await studentCollection.find().toArray();
+  let resObj = {
+    status: 1,
+    msg: "studentinf",
+    data,
+  };
+  res.send(resObj);
 
-app.post("/student-insert", async(req,res)=>{
-  let myDB = await dbConnection()
-  let studentCollection = myDB.collection("student")
+  res.send("App vieW API");
+});
+
+app.post("/student-insert", async (req, res) => {
+  let myDB = await dbConnection();
+  let studentCollection = myDB.collection("student");
 
   // let obj={
   //   sname : req.body.name,
   //   semail: req.body.email
   // }
 
-  let {name,email} = req.body;
-  let obj = {name,email }
-  let insertRes = await studentCollection.insertOne(obj)
+  let { name, email } = req.body;
+  let obj = { name, email };
+  let insertRes = await studentCollection.insertOne(obj);
 
-  let resObj={
+  let resObj = {
     status: 1,
     msg: "Data insert",
-    insertRes
-  }
-  res.send(resObj)
+    insertRes,
+  };
+  res.send(resObj);
 
-  console.log(obj)
-  res.send("App insert API")
+  console.log(obj);
+  res.send("App insert API");
+});
 
-})
-
-app.listen("8000")
-
-
+app.listen("8000");
